b1f84aa36c4fe4139dc348df0b409f7edfa83f89
George Chen <georgechen1338@gmail.com>
2022-04-06 15:42:15 -0400

    saved assertion type

--- seed/data.js
+++ seed/data.js
@@ -1,7 +1,7 @@
 //set up all required files
 import db from "../database/connection.js";
 import Character from "../models/characters.js";
-import characters from "./characters.json";
+import characters from "./characters.json" assert { type: "json" };
 
 const setUp = async () => {
   db.dropDatabase();
